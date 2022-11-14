894807009d83e51d593801c85cd2788db124e5f7
3ef1rus <10kmmrza10k@mail.ru>
2022-11-14 21:58:11 +0300

    complete gulp

---
+++ dist/all.js
@@ -0,0 +1,14 @@
+function print(){
+    return console.log('file 2');
+}
+
+print();
+function print(){
+    return console.log('file 3');
+}
+print();
+
+function print(){
+    return console.log('file 1');
+}
+print();
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var concat = require('gulp-concat');
- 
+var gulp = require('gulp');
 gulp.task('scripts', function() {
   return gulp.src('./lib/*.js')
     .pipe(concat('all.js'))
